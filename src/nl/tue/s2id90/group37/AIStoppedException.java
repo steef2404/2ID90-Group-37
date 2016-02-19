496d67c1a7c42572d87b6688d8f3473ad87724c4
steef2404 <steef2404@hotmail.com>
2016-02-19 17:24:42 +0100

    Revert "Revert "itnro""
    This reverts commit 91ea7fc1672ed267cc961d87f8875b43de87ede4.

---
+++ src/nl/tue/s2id90/group37/AIStoppedException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.tue.s2id90.group37;
+
+/**
+ *
+ * @author s130973
+ */
+public class AIStoppedException extends Exception {
+    
+}
