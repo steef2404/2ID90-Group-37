c9a3eabac47246d6a87447192e9724b29a487d2f
steef2404 <s130973@S130973.campus.tue.nl>
2016-02-29 10:14:11 +0100

    most important change!!!
    optimistic en bowlinbal omgedraaid.
    EN het BELANGRIJKSTE het plaatje veranderd naar een BOWLINGBAL

--- src/nl/tue/s2id90/group37/Bowlingbal.java
+++ src/nl/tue/s2id90/group37/Bowlingbal.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.tue.s2id90.group37;
 
 import java.util.List;
@@ -12,15 +7,16 @@ import nl.tue.s2id90.game.GameState;
 import org10x10.dam.game.Move;
 
 /**
+ * A simple draughts player that plays the first moves that comes to mind and
+ * values all moves with value 0.
  *
- * @author s130973
+ * @author s139073
  */
 public class Bowlingbal extends DraughtsPlayer {
 
     public Bowlingbal() {
-        super(Bowlingbal.class.getResource("resources/smiley.png"));
+        super(Bowlingbal.class.getResource("resources/bowlingbal.PNG"));
     }
-
     private boolean stopped;
 
     /*
@@ -31,89 +27,14 @@ public class Bowlingbal extends DraughtsPlayer {
         stopped = true;
     }
 
-    int value = 0;  //value in GUI
+    int value = 0;
 
-    /*
-     Method to set value in GUI
-     */
-    @Override
-    public Integer getValue() {
-        return value; //To change body of generated methods, choose Tools | Templates.
-    }
-
-    /*
-     AlphaBeta method for recursive searching
-     */
-    int alphaBeta(GameNode node, int alpha, int beta, int depth, int limit, boolean maximize)
-            throws AIStoppedException {
-        if (stopped) {  //interrupt
-            stopped = false;
-            throw new AIStoppedException();
-        }
-        GameState state = node.getGameState();      //get state
-
-        if (state.isEndState() || depth > limit) {  //if it is the last state or the depth is over the limit return last evaluate 
-            return evaluate((DraughtsState) state);
-        }
-
-        List<Move> moves = state.getMoves();        //get all moves
-        Move bestMove = moves.get(0);               //set best move random
-
-//        if (moves.size() == 1) {                    //if only 1 move is available do it
-//            System.out.println("1 zet");
-//            return evaluate((DraughtsState) state);
-//        }
-        if (maximize) {
-            int temp = Integer.MIN_VALUE;
-            for (Move move : moves) {
-                state.doMove(move);
-                GameNode newNode = new GameNode(state);
-                temp = Math.max(temp, alphaBeta(newNode, alpha, beta, depth + 1, limit, false)); //recursive call
-                if (temp > alpha) {
-                    alpha = temp;
-                    bestMove = move;
-                }
-                state.undoMove(move);
-                if (beta <= alpha) {
-                    node.setBestMove(bestMove);
-                    return beta;
-                }
-            }
-            node.setBestMove(bestMove);
-            return alpha;
-        } else {
-            int temp = Integer.MAX_VALUE;
-            for (Move move : moves) {
-                state.doMove(move);
-                GameNode newNode = new GameNode(state);
-                temp = Math.min(temp, alphaBeta(newNode, alpha, beta, depth + 1, limit, true));        //recursive call
-                beta = Math.min(beta, temp);
-                state.undoMove(move);
-                if (beta <= alpha) {
-                    node.setBestMove(bestMove);
-                    return alpha;
-                }
-            }
-            node.setBestMove(bestMove);
-            return beta;
-        }
-    }
-
-    /*
-     Method to evaluate the draughts
-     */
-    int evaluate(DraughtsState ds) {
-//obtain pieces array
+    public int evaluate(DraughtsState ds) {
         int[] pieces = ds.getPieces();
         int computedValue = 0;
         int count = 0;
-
-        // computedValue = countPieces(ds);
-        //      computedValue += endState(ds);
-        for (int k = 1; k < pieces.length; k++) {       //go to the middle
-
+        for (int k = 1; k < pieces.length; k++) {
             if (!isEndgame) {
-               // System.out.println("niet endgame");
                 if (pieces[k] == 1) {
                     count++;
                     computedValue += 40;
@@ -138,61 +59,8 @@ public class Bowlingbal extends DraughtsPlayer {
                         computedValue -= 150;
                     }
                 }
-                if (22 <= k && k <= 24 || 27 <= k && k <= 29) {
-                    if (pieces[k] == 1) {
-                        computedValue++;
-                    }
-                    if (pieces[k] == 2) {
-                        computedValue--;
-                    }
-                }
-
-                if (k % 10 == 1 || k % 10 == 2 || k % 10 == 3 || k % 10 == 4) {    //check if you are covered from white side
-                    if (isWhite) {
-                        if (pieces[k] == 1) {
-                            if (pieces[k + 5] == 1 && pieces[k + 6] == 1) {
-                                computedValue += 2;
-                            }
-                        }
-                    } else if (k > 5) {
-                        if (pieces[k] == 2) {
-                            if (pieces[k - 4] == 2 && pieces[k - 5] == 2) {
-                                computedValue -= 2;
-                            }
-                        }
-                    }
-                }
-
-                if (k % 10 == 7 || k % 10 == 8 || k % 10 == 9 || k % 10 == 0) {    //check if you are covered from black side
-                    if (!isWhite) {
-                        if (pieces[k] == 2) {
-                            if (pieces[k - 5] == 2 && pieces[k - 6] == 2) {
-                                computedValue -= 2;
-                            }
-                        }
-                    } else if (k < 47) {
-                        if (pieces[k] == 1) {
-                            if (pieces[k + 4] == 1 && pieces[k + 5] == 1) {
-                                computedValue += 2;
-                            }
-                        }
-                    }
-                }
-
-//            if (pieces[k] == 1) {
-//                computedValue += 20;
-//            }
-//            if (pieces[k] == 2) {
-//                computedValue -= 20;
-//            }
-//            if (pieces[k] == 3) {
-//                computedValue += 50;
-//            }
-//            if (pieces[k] == 4) {
-//                computedValue -= 50;
-//            }
-            } else {
-                //System.out.println("end game");
+            }
+            else {
                 if (pieces[k] == 1) {
                     computedValue += 40;
                 }
@@ -243,35 +111,71 @@ public class Bowlingbal extends DraughtsPlayer {
         if (!isWhite) {
             computedValue = -computedValue;
         }
-
-        //System.out.println("computedValue = " + computedValue);
         return computedValue;
     }
-    //    public int countPieces(DraughtsState ds) {
-    //        int score = 0;
-    //        
-    //        int[] pieces = ds.getPieces();
-    //        for (int k = 1; k < pieces.length; k++) {
-    //
-    //        return score;
-    //    }
 
-    public int endState(DraughtsState ds) {
-        int win = 0;
-        if ((isWhite != ds.isWhiteToMove()) && ds.isEndState()) {
-            win += 100000;
+    int alphaBeta(GameNode node, int alpha, int beta, int depth, int limit, boolean maximize)
+            throws AIStoppedException {
+        if (stopped) {  //interrupt
+            stopped = false;
+            throw new AIStoppedException();
         }
-//        if ((isWhite == ds.isWhiteToMove()) && ds.isEndState()) {
-//            win -= 100000;
+        GameState state = node.getGameState();      //get state
+
+        if (state.isEndState() || depth > limit) {  //if it is the last state or the depth is over the limit return last evaluate 
+            return evaluate((DraughtsState) state);
+        }
+
+        List<Move> moves = state.getMoves();        //get all moves
+        Move bestMove = moves.get(0);               //set best move random
+
+//        if (moves.size() == 1) {
+//            return evaluate((DraughtsState) state);
 //        }
-        return win;
+        if (maximize) {
+            int temp = Integer.MIN_VALUE;
+            for (Move move : moves) {
+                state.doMove(move);
+                GameNode newNode = new GameNode(state);
+                temp = Math.max(temp, alphaBeta(newNode, alpha, beta, depth + 1, limit, false)); //recursive call
+                if (temp > alpha) {
+                    alpha = temp;
+                    bestMove = move;
+                }
+                state.undoMove(move);
+                if (beta <= alpha) {
+                    node.setBestMove(bestMove);
+                    return beta;
+                }
+            }
+            node.setBestMove(bestMove);
+            return alpha;
+        } else {
+            int temp = Integer.MAX_VALUE;
+            for (Move move : moves) {
+                state.doMove(move);
+                GameNode newNode = new GameNode(state);
+                temp = Math.min(temp, alphaBeta(newNode, alpha, beta, depth + 1, limit, true));        //recursive call
+                beta = Math.min(beta, temp);
+                state.undoMove(move);
+                if (beta <= alpha) {
+                    node.setBestMove(bestMove);
+                    return alpha;
+                }
+            }
+            node.setBestMove(bestMove);
+            return beta;
+        }
     }
 
-    Move bestMove;          //best move
-    Boolean isWhite;        //player is white or black
-    Boolean isEndgame = false;       //see if it is endgame
+    Move bestMove;
+    Boolean isWhite;
+    Boolean isEndgame = false;
 
     @Override
+    /**
+     * @return a random move *
+     */
     public Move getMove(DraughtsState s) {
         GameNode node = new GameNode(s.clone());
         isWhite = s.isWhiteToMove();
@@ -282,7 +186,7 @@ public class Bowlingbal extends DraughtsPlayer {
                 value = alphaBeta(node, Integer.MIN_VALUE, Integer.MAX_VALUE, 0, limit, true); //find best move
                 //System.out.println("value: " + value);
                 limit++;
-                System.out.println("limit: " + limit);
+                System.out.println("limit bowlingbal: " + limit);
                 if (node.getBestMove() == null) {           //never get here
                     System.out.println("null move");
                     List<Move> moves = node.getGameState().getMoves();
@@ -295,5 +199,10 @@ public class Bowlingbal extends DraughtsPlayer {
             //do nothing
         }
         return bestMove;
+    }
+
+    @Override
+    public Integer getValue() {
+        return value;
     }
 }
--- src/nl/tue/s2id90/group37/OptimisticPlayer.java
+++ src/nl/tue/s2id90/group37/OptimisticPlayer.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package nl.tue.s2id90.group37;
 
 import java.util.List;
@@ -7,16 +12,15 @@ import nl.tue.s2id90.game.GameState;
 import org10x10.dam.game.Move;
 
 /**
- * A simple draughts player that plays the first moves that comes to mind and
- * values all moves with value 0.
  *
  * @author huub
  */
 public class OptimisticPlayer extends DraughtsPlayer {
 
     public OptimisticPlayer() {
-        super(UninformedPlayer.class.getResource("resources/optimist.png"));
+        super(OptimisticPlayer.class.getResource("resources/smiley.png"));
     }
+
     private boolean stopped;
 
     /*
@@ -27,14 +31,89 @@ public class OptimisticPlayer extends DraughtsPlayer {
         stopped = true;
     }
 
-    int value = 0;
+    int value = 0;  //value in GUI
 
-    public int evaluate(DraughtsState ds) {
+    /*
+     Method to set value in GUI
+     */
+    @Override
+    public Integer getValue() {
+        return value; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /*
+     AlphaBeta method for recursive searching
+     */
+    int alphaBeta(GameNode node, int alpha, int beta, int depth, int limit, boolean maximize)
+            throws AIStoppedException {
+        if (stopped) {  //interrupt
+            stopped = false;
+            throw new AIStoppedException();
+        }
+        GameState state = node.getGameState();      //get state
+
+        if (state.isEndState() || depth > limit) {  //if it is the last state or the depth is over the limit return last evaluate 
+            return evaluate((DraughtsState) state);
+        }
+
+        List<Move> moves = state.getMoves();        //get all moves
+        Move bestMove = moves.get(0);               //set best move random
+
+//        if (moves.size() == 1) {                    //if only 1 move is available do it
+//            System.out.println("1 zet");
+//            return evaluate((DraughtsState) state);
+//        }
+        if (maximize) {
+            int temp = Integer.MIN_VALUE;
+            for (Move move : moves) {
+                state.doMove(move);
+                GameNode newNode = new GameNode(state);
+                temp = Math.max(temp, alphaBeta(newNode, alpha, beta, depth + 1, limit, false)); //recursive call
+                if (temp > alpha) {
+                    alpha = temp;
+                    bestMove = move;
+                }
+                state.undoMove(move);
+                if (beta <= alpha) {
+                    node.setBestMove(bestMove);
+                    return beta;
+                }
+            }
+            node.setBestMove(bestMove);
+            return alpha;
+        } else {
+            int temp = Integer.MAX_VALUE;
+            for (Move move : moves) {
+                state.doMove(move);
+                GameNode newNode = new GameNode(state);
+                temp = Math.min(temp, alphaBeta(newNode, alpha, beta, depth + 1, limit, true));        //recursive call
+                beta = Math.min(beta, temp);
+                state.undoMove(move);
+                if (beta <= alpha) {
+                    node.setBestMove(bestMove);
+                    return alpha;
+                }
+            }
+            node.setBestMove(bestMove);
+            return beta;
+        }
+    }
+
+    /*
+     Method to evaluate the draughts
+     */
+    int evaluate(DraughtsState ds) {
+//obtain pieces array
         int[] pieces = ds.getPieces();
         int computedValue = 0;
         int count = 0;
-        for (int k = 1; k < pieces.length; k++) {
+
+        // computedValue = countPieces(ds);
+        //      computedValue += endState(ds);
+        for (int k = 1; k < pieces.length; k++) {       //go to the middle
+
             if (!isEndgame) {
+               // System.out.println("niet endgame");
                 if (pieces[k] == 1) {
                     count++;
                     computedValue += 40;
@@ -59,8 +138,61 @@ public class OptimisticPlayer extends DraughtsPlayer {
                         computedValue -= 150;
                     }
                 }
-            }
-            else {
+                if (22 <= k && k <= 24 || 27 <= k && k <= 29) {
+                    if (pieces[k] == 1) {
+                        computedValue++;
+                    }
+                    if (pieces[k] == 2) {
+                        computedValue--;
+                    }
+                }
+
+                if (k % 10 == 1 || k % 10 == 2 || k % 10 == 3 || k % 10 == 4) {    //check if you are covered from white side
+                    if (isWhite) {
+                        if (pieces[k] == 1) {
+                            if (pieces[k + 5] == 1 && pieces[k + 6] == 1) {
+                                computedValue += 2;
+                            }
+                        }
+                    } else if (k > 5) {
+                        if (pieces[k] == 2) {
+                            if (pieces[k - 4] == 2 && pieces[k - 5] == 2) {
+                                computedValue -= 2;
+                            }
+                        }
+                    }
+                }
+
+                if (k % 10 == 7 || k % 10 == 8 || k % 10 == 9 || k % 10 == 0) {    //check if you are covered from black side
+                    if (!isWhite) {
+                        if (pieces[k] == 2) {
+                            if (pieces[k - 5] == 2 && pieces[k - 6] == 2) {
+                                computedValue -= 2;
+                            }
+                        }
+                    } else if (k < 47) {
+                        if (pieces[k] == 1) {
+                            if (pieces[k + 4] == 1 && pieces[k + 5] == 1) {
+                                computedValue += 2;
+                            }
+                        }
+                    }
+                }
+
+//            if (pieces[k] == 1) {
+//                computedValue += 20;
+//            }
+//            if (pieces[k] == 2) {
+//                computedValue -= 20;
+//            }
+//            if (pieces[k] == 3) {
+//                computedValue += 50;
+//            }
+//            if (pieces[k] == 4) {
+//                computedValue -= 50;
+//            }
+            } else {
+                //System.out.println("end game");
                 if (pieces[k] == 1) {
                     computedValue += 40;
                 }
@@ -111,71 +243,35 @@ public class OptimisticPlayer extends DraughtsPlayer {
         if (!isWhite) {
             computedValue = -computedValue;
         }
+
+        //System.out.println("computedValue = " + computedValue);
         return computedValue;
     }
+    //    public int countPieces(DraughtsState ds) {
+    //        int score = 0;
+    //        
+    //        int[] pieces = ds.getPieces();
+    //        for (int k = 1; k < pieces.length; k++) {
+    //
+    //        return score;
+    //    }
 
-    int alphaBeta(GameNode node, int alpha, int beta, int depth, int limit, boolean maximize)
-            throws AIStoppedException {
-        if (stopped) {  //interrupt
-            stopped = false;
-            throw new AIStoppedException();
+    public int endState(DraughtsState ds) {
+        int win = 0;
+        if ((isWhite != ds.isWhiteToMove()) && ds.isEndState()) {
+            win += 100000;
         }
-        GameState state = node.getGameState();      //get state
-
-        if (state.isEndState() || depth > limit) {  //if it is the last state or the depth is over the limit return last evaluate 
-            return evaluate((DraughtsState) state);
-        }
-
-        List<Move> moves = state.getMoves();        //get all moves
-        Move bestMove = moves.get(0);               //set best move random
-
-//        if (moves.size() == 1) {
-//            return evaluate((DraughtsState) state);
+//        if ((isWhite == ds.isWhiteToMove()) && ds.isEndState()) {
+//            win -= 100000;
 //        }
-        if (maximize) {
-            int temp = Integer.MIN_VALUE;
-            for (Move move : moves) {
-                state.doMove(move);
-                GameNode newNode = new GameNode(state);
-                temp = Math.max(temp, alphaBeta(newNode, alpha, beta, depth + 1, limit, false)); //recursive call
-                if (temp > alpha) {
-                    alpha = temp;
-                    bestMove = move;
-                }
-                state.undoMove(move);
-                if (beta <= alpha) {
-                    node.setBestMove(bestMove);
-                    return beta;
-                }
-            }
-            node.setBestMove(bestMove);
-            return alpha;
-        } else {
-            int temp = Integer.MAX_VALUE;
-            for (Move move : moves) {
-                state.doMove(move);
-                GameNode newNode = new GameNode(state);
-                temp = Math.min(temp, alphaBeta(newNode, alpha, beta, depth + 1, limit, true));        //recursive call
-                beta = Math.min(beta, temp);
-                state.undoMove(move);
-                if (beta <= alpha) {
-                    node.setBestMove(bestMove);
-                    return alpha;
-                }
-            }
-            node.setBestMove(bestMove);
-            return beta;
-        }
+        return win;
     }
 
-    Move bestMove;
-    Boolean isWhite;
-    Boolean isEndgame = false;
+    Move bestMove;          //best move
+    Boolean isWhite;        //player is white or black
+    Boolean isEndgame = false;       //see if it is endgame
 
     @Override
-    /**
-     * @return a random move *
-     */
     public Move getMove(DraughtsState s) {
         GameNode node = new GameNode(s.clone());
         isWhite = s.isWhiteToMove();
@@ -199,10 +295,5 @@ public class OptimisticPlayer extends DraughtsPlayer {
             //do nothing
         }
         return bestMove;
-    }
-
-    @Override
-    public Integer getValue() {
-        return value;
     }
 }
